fbdd8515d9e0dd0b7d6afba6dab4d9209323e028
Dima <celischev85@gmail.com>
2025-11-24 11:30:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int Balance = 300;
+        int amountUp = 1500;
+
+
+        int bonus = 0;
+        int totalBalance = 0;
+
+        if (amountUp >= 1000) {
+            bonus = (amountUp / 100);
+        }
+
+        totalBalance = Balance + bonus + amountUp;
+
+        if (bonus > 0) {
+            System.out.println("Начислено " + bonus + " бонусных руб.");
+            System.out.println("Итоговый баланс: " + totalBalance + " руб.");
+        } else {
+            System.out.println("Бонусные рубли отсутствуют.");
+            System.out.println("Итоговый баланс: " + totalBalance + " руб.");
+        }
+    }
+}
